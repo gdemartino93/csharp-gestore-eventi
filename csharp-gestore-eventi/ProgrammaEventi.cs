b04497a7405c57512166b8ff17cb436763ac63bd
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-08 15:29:52 +0200

    create new class

---
+++ csharp-gestore-eventi/ProgrammaEventi.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_gestore_eventi
+{
+    internal class ProgrammaEventi
+    {
+        private string titolo;
+        List<Evento> eventi;
+
+        public string Titolo { get; set; }
+        public List<Evento> Eventi { get; set; }
+
+        public ProgrammaEventi(string titolo)
+        {
+            Titolo = titolo;
+            Eventi = new List<Evento>();
+        }
+    }
+}
