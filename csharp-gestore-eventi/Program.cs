bfa32f62aaf5fe6eaec3d4f5a9626553d6b427f3
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-08 16:37:13 +0200

    debug

--- csharp-gestore-eventi/Program.cs
+++ csharp-gestore-eventi/Program.cs
@@ -4,6 +4,27 @@
     {
         static void Main(string[] args)
         {
+
+            ProgrammaEventi eventi = new ProgrammaEventi("lista eventi");
+
+
+
+            Evento uno = new Evento("concerto1", new DateTime(2024,12,01), 100);
+            Evento due = new Evento("concerto2", new DateTime(2024,12,02), 200);
+            Evento tre = new Evento("concerto3", new DateTime(2024,12,03), 300);
+            Evento quattro = new Evento("concerto4", new DateTime(2024, 12, 03), 300);
+            Evento cinque = new Evento("concerto5", new DateTime(2024, 12, 03), 300);
+            eventi.aggiungiEvento(uno);
+            eventi.aggiungiEvento(due);
+            eventi.aggiungiEvento(tre);
+            eventi.aggiungiEvento(quattro);
+            eventi.aggiungiEvento(cinque);
+
+            eventi.eventoPerData(new DateTime(2024, 12, 03));
+
+       
+           
+
             Console.WriteLine("Crea evento");
             Console.WriteLine("Inserisci il titolo dell'evento:");
             string nomeEvento = Console.ReadLine();
