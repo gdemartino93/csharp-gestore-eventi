6ee0ae593f13522d32b0924e22cfbd165d3d455b
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-08 18:07:32 +0200

    create new class conference extended from event

---
+++ csharp-gestore-eventi/Conferenza.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_gestore_eventi
+{
+    internal class Conferenza:Evento
+    {
+        private string relatore;
+        private double prezzo;
+
+        public string Relatore { get; set; }
+        public double Prezzo { get; set; }
+
+        public Conferenza(string titolo,DateTime data,int capienzaMassima,string relatore, double prezzo):base(titolo,data,capienzaMassima)
+        {
+            Relatore = relatore;
+            Prezzo = prezzo;
+        }
+    }
+}
